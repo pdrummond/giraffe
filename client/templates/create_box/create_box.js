8dafe0e4603a362f884dc12c3922bc9903888c82
Paul Drummond <paul.drummond@iode.co.uk>
2015-06-04 21:48:00 +0100

    Initial support for markdown and improved createbox

--- client/templates/create_box/create_box.js
+++ client/templates/create_box/create_box.js
@@ -1,18 +1,22 @@
 Template.createBox.events({
-  'submit form': function(e) {
-    e.preventDefault();
+  "keydown #create-box-input": function(ev) {
+    if(ev.keyCode == 13 && ev.shiftKey == false) {
+      ev.preventDefault();
+      var content = $('#create-box-input').val();
+      if(content.length > 0) {
+        var message = {
+          content: content
+        };
 
-    var message = {
-      content: $(e.target).find('[name=content]').val()
-    };
-
-    Meteor.call('messageInsert', message, function(error, result) {
-      if (error) {
-        return alert(error.reason);
-      } else {
-        $(e.target).find('[name=content]').val("");
+        Meteor.call('messageInsert', message, function(error, result) {
+          if (error) {
+            return alert(error.reason);
+          } else {
+            window.scrollTo(0,document.body.scrollHeight);
+            $('#create-box-input').val("");
+          }
+        });
       }
-    });
-
+    }
   }
 });
