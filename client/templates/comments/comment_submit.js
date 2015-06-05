57f754561fdaff1c7c8bc908b433d4470f5527c3
unknown <UKX7675@GBLH251195.NissanEU.NMCorp.Nissan.Biz>
2015-06-05 16:30:16 +0100

    Added comment submit

---
+++ client/templates/comments/comment_submit.js
@@ -0,0 +1,39 @@
+Template.commentSubmit.onCreated(function() {
+  Session.set('commentSubmitErrors', {});
+});
+
+Template.commentSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('commentSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('commentSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.commentSubmit.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+
+    var $body = $(e.target).find('[name=body]');
+    var comment = {
+        messageType: "commentMessage",
+      content: $body.val(),
+      parentMessageId: template.data._id
+    };
+
+    var errors = {};
+    if (! comment.content) {
+      errors.body = "Please write some content";
+      return Session.set('commentSubmitErrors', errors);
+    }
+
+    Meteor.call('messageInsert', comment, function(error, messageId) {
+      if (error){
+        alert(error.reason);
+      } else {
+        $body.val('');
+      }
+    });
+  }
+});
