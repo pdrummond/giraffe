8bb473ca50335664655460fa25c40bc3306a9530
Paul Drummond <paul.drummond@iode.co.uk>
2015-06-08 16:30:32 +0100

    Added filter dropdown button

--- client/templates/search_box/search_box.js
+++ client/templates/search_box/search_box.js
@@ -4,6 +4,7 @@ SearchBoxComponent = BlazeComponent.extendComponent({
 
   events: function() {
     return [{
+      "click .filter-command": this.onFilterCommand,
       "keydown #search-box-input": this.onKeydown,
     }];
   },
@@ -12,12 +13,23 @@ SearchBoxComponent = BlazeComponent.extendComponent({
     return Session.get('filterQuery');
   },
 
+  onFilterCommand: function(e) {
+    e.preventDefault();
+    var filterQuery = $(e.target).text();
+    this.setQuery(filterQuery);
+  },
+
   onKeydown: function(ev) {
     if(ev.keyCode == 13) {
       ev.preventDefault();
       var filterQuery = $('#search-box-input').val();
-      Session.setPersistent('filterQuery', filterQuery);
-      Meteor.subscribe('messages', {filterQuery: filterQuery});
+      this.setQuery(filterQuery);
     }
+  },
+
+  setQuery: function(filterQuery) {
+    Session.setPersistent('filterQuery', filterQuery);
+    Meteor.subscribe('messages', {filterQuery: filterQuery});
   }
+
 }).register('SearchBoxComponent');
